a6339dfed7eb5781b3a42bb357efc0d52be2bf0f
Gopi Gorantala <gopishiva001@outlook.com>
2022-04-30 17:37:47 +0530

    initial commit to bit manipulation repository

---
+++ src/CountDigits.java
@@ -0,0 +1,47 @@
+public class CountDigits {
+  public static void main(String[] args) {
+    int number = 125;
+    System.out.println("Number of digits : " + countDigits(number)); // recommended approach
+    System.out.println("Number of digits : " + logarithmicApproach(number));
+    System.out.println("Number of digits : " + recursiveApproach(number));
+    System.out.println("Number of digits : " + stringApproach(number));
+  }
+
+  // Optimal approach: Division
+  // Time: O(n), run time depends on the number of digits in n. In the worst case, it iterates
+  // through all the digits until it becomes 0.
+  // Space: O(1), space complexity is O(1) since no additional space is allocated.
+  private static int countDigits(int n) {
+    int count = 0;
+
+    while (n > 0) {
+      ++count;
+      n /= 10;
+    }
+
+    return count;
+  }
+
+  // logarithmic approach
+  private static int logarithmicApproach(int n) {
+    return n != 0 ? ((int) Math.floor(Math.log10(n) + 1)) : -1;
+  }
+
+  // recursive approach
+  private static int recursiveApproach(int n) {
+    // base checks
+    if (n == 0) {
+      return 0;
+    }
+
+    return (1 + recursiveApproach(n / 10));
+  }
+
+  // string conversion
+  private static int stringApproach(int n) {
+    // you can also use String.valueOf(number) to convert int to string.
+    String num = Integer.toString(n);
+
+    return num.length();
+  }
+}
---
+++ src/DecimalToBinary.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DecimalToBinary {
+  public static void main(String[] args) {
+    int number = 125;
+    System.out.println("Number of bits : " + bitShifting(number));
+    System.out.println("Number of bits : " + bitShiftingOptimal(number));
+    System.out.println("Number of bits : " + decimalToBinaryUsingStack(number));
+  }
+
+  // bit shifting approach
+  private static int bitShifting(int n) {
+    int bitsCounter = 0;
+
+    while ((1 << bitsCounter) <= n) {
+      bitsCounter += 1;
+    }
+
+    return bitsCounter;
+  }
+
+  // optimal solution - bit shifting
+  private static int bitShiftingOptimal(int n) {
+    int bitsCounter = 0;
+
+    while ((1 << bitsCounter) <= n) {
+      bitsCounter += 1;
+    }
+
+    return bitsCounter;
+  }
+
+  // using Stack approach
+  private static String decimalToBinaryUsingStack(int n) {
+    Stack<Integer> stack = new Stack<>();
+
+    while (n > 0) {
+      int remainder = n % 2; // remainder gives either 0 OR 1
+      stack.push(remainder); // store the remainder value in stack
+      n >>= 1; // this is equivalent to n = n/2;
+    }
+
+    // loggers
+    Arrays.asList("while loop breaks only when \"number\" terminates to : " + n, "  ")
+        .forEach(System.out::println);
+
+    int[] bits = new int[stack.size()];
+    int k = 0;
+
+    while (!stack.isEmpty()) {
+      // each bit is popped and stored in array of integers
+      bits[k++] = stack.pop();
+    }
+
+    return Arrays.toString(bits);
+  }
+}
