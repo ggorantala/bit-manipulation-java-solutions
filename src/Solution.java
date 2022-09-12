f73c5600bd513da505b418fac8f7fb5ee1dc3286
Gopi Gorantala <gopishiva001@outlook.com>
2022-09-12 16:19:23 +0530

    reverse a list

---
+++ src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+  public static void main(String[] args) {
+    List<Integer> A = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    System.out.println(solve(A));
+  }
+
+  // DO NOT MODIFY THE LIST. IT IS READ ONLY
+  public static ArrayList<Integer> solve(List<Integer> A) {
+    int i = 0;
+    int j = A.size() - 1;
+
+    while (i < j) {
+      int temp = A.get(i);
+      A.set(i, A.get(j));
+      A.set(j, temp);
+
+      i++;
+      j--;
+    }
+
+    return (ArrayList<Integer>) A;
+  }
+}
